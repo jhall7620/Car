9deab9adb014e36152825188692a2bc1c49d571b
jhall7620 <jhall7620@gmail.com>
2023-09-16 09:43:25 -0400

    setup server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+// Dependencies
+const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+
+// Configuration
+const PORT = process.env.PORT
+const app = express()
+
+// Middleware
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+// db connection
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err))
+
+// 404 page
+app.get('*', (req, res) => {
+    res.send(404)
+})
+
+// Listen
+app.listen(PORT, console.log(`listening on port ${PORT}`))
